20c9a953e31230a80bd929401a398554ed218734
Ken Conley <kwc@willowgarage.com>
2011-09-12 20:09:44 -0700

    missing files

---
+++ setup.sh
@@ -0,0 +1,10 @@
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if([ -h "${SCRIPT_PATH}" ]) then
+  while([ -h "${SCRIPT_PATH}" ]) do SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+fi
+pushd . > /dev/null
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+SCRIPT_PATH=`pwd`;
+popd  > /dev/null
+
+export PYTHONPATH=$SCRIPT_PATH/src:$PYTHONPATH
