574fc21ee230ff3b14f3780b88525a63bb19c80a
Ken Conley <kwc@kwc.org>
2012-02-22 10:43:51 -0800

    0.8.1

--- src/rosdep2/__init__.py
+++ src/rosdep2/__init__.py
@@ -33,7 +33,7 @@ rosdep library and command-line tool
 
 from __future__ import print_function
 
-__version__ = '0.8.0'
+__version__ = '0.8.1'
 
 from .installers import InstallerContext, Installer, PackageManagerInstaller
 
