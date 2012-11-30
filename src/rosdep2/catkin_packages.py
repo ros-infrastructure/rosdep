df157f7dfc20c1145c3618330e8689e32c1be7dc
William Woodall <wjwwood@gmail.com>
2012-11-30 11:27:31 -0800

    Added catkin_packages module.
    This new module contains code for finding catkin
    packages in arbitrary folders and storing the
    results.

---
+++ src/rosdep2/catkin_packages.py
@@ -0,0 +1,72 @@
+"""
+Helper routines for catkin.  These are distributed inside of rosdep2
+to protect catkin against future rosdep2 API updatese.  These helper
+routines are assumed to run in an interactive mode with an end-user
+and thus return end-user oriented error messages.
+
+Errors are returned as arguments to raised :exc:`ValidationFailed`
+exceptions.
+
+Workflow::
+
+    installer = get_installer(APT_INSTALLER)
+    view = get_catkin_view(rosdistro_name, 'ubuntu', 'lucid')
+    resolve_for_os(rosdep_key, view, installer, 'ubuntu', 'lucid')
+
+"""
+
+from __future__ import print_function
+
+import os
+import sys
+
+try:
+    from catkin_pkg.packages import find_packages
+except ImportError:
+    print("catkin_pkg was not detected, please install it.",
+          file=sys.stderr)
+    sys.exit(1)
+
+_catkin_workspace_packages = []
+_catkin_packages_cache = {}
+
+
+def find_catkin_packages_in(path, verbose=False):
+    """
+    :returns: a list of packages in a given directory
+    :raises: OSError if the path doesn't exist
+    """
+    global _catkin_packages_cache
+    if not os.path.exists(path):
+        raise OSError("given path '{0}' does not exist".format(path))
+    if verbose:
+        print("Looking for packages in '{0}'... ".format(path),
+              end='', file=sys.stderr)
+    path = os.path.abspath(path)
+    if path in _catkin_packages_cache:
+        if verbose:
+            print("found in cache.", file=sys.stderr)
+        return _catkin_packages_cache[path]
+    packages = find_packages(path)
+    if type(packages) == dict and packages != {}:
+        package_names = [package.name for package in packages.values()]
+        if verbose:
+            print("found " + str(len(packages)) + " packages.")
+            for package in package_names:
+                print("    {0}".format(package))
+        _catkin_packages_cache[path] = package_names
+        return package_names
+    else:
+        if verbose:
+            print("failed to find packages.", file=sys.stderr)
+        return []
+
+
+def set_workspace_packages(packages):
+    global _catkin_workspace_packages
+    _catkin_workspace_packages = list(packages)
+
+
+def get_workspace_packages():
+    global _catkin_workspace_packages
+    return _catkin_workspace_packages
