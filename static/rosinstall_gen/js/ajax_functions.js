a4c7285b0844d9e8454346f18209983806cf7b97
Eitan Marder-Eppstein <eitan@hidof.com>
2013-01-29 20:23:55 -0800

    Updating js

--- static/rosinstall_gen/js/ajax_functions.js
+++ static/rosinstall_gen/js/ajax_functions.js
@@ -1,10 +1,10 @@
-function get_rosinstall(distro, packages, dry)
+function get_rosinstall(distro, packages, gen_type)
 {
   console.log("Building rosinstall for: " + distro + ", " + packages);
   Dajaxice.prerelease_website.rosinstall_gen.get_rosinstall_ajax(rosinstall_cb, 
 		                                                 {'distro': distro,
 							          'packages': packages,
-								  'dry': dry});
+								  'gen_type': gen_type});
 }
 
 function rosinstall_cb(data)
