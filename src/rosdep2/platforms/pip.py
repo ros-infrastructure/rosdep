c4c3d6c648c92f035a883caf6160f34584e6a892
William Woodall <william@osrfoundation.org>
2015-01-26 16:52:06 -0800

    [style] pip.py: cleanup whitespace and PEP8

--- src/rosdep2/platforms/pip.py
+++ src/rosdep2/platforms/pip.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # Copyright (c) 2009, Willow Garage, Inc.
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above copyright
@@ -13,7 +13,7 @@
 #     * Neither the name of the Willow Garage, Inc. nor the names of its
 #       contributors may be used to endorse or promote products derived from
 #       this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -39,8 +39,10 @@ from ..shell_utils import read_stdout
 # pip package manager key
 PIP_INSTALLER = 'pip'
 
+
 def register_installers(context):
     context.set_installer(PIP_INSTALLER, PipInstaller())
+
 
 def is_pip_installed():
     try:
@@ -49,8 +51,9 @@ def is_pip_installed():
     except OSError:
         return False
 
+
 def pip_detect(pkgs, exec_fn=None):
-    """ 
+    """
     Given a list of package, return the list of installed packages.
 
     :param exec_fn: function to execute Popen and read stdout (for testing)
@@ -66,8 +69,9 @@ def pip_detect(pkgs, exec_fn=None):
             ret_list.append(pkg_row[0])
     return ret_list
 
+
 class PipInstaller(PackageManagerInstaller):
-    """ 
+    """
     :class:`Installer` support for pip.
     """
 
@@ -82,4 +86,3 @@ class PipInstaller(PackageManagerInstaller):
             return []
         else:
             return [self.elevate_priv(['pip', 'install', '-U', p]) for p in packages]
-            
