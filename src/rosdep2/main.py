b50a05e130798a93ed0d3d7b2f53d7c8cd3f2701
William Woodall <william@osrfoundation.org>
2015-05-18 10:55:37 -0700

    [style] line length

--- src/rosdep2/main.py
+++ src/rosdep2/main.py
@@ -625,15 +625,21 @@ def command_install(lookup, packages, options):
             return 1
     else:
         uninstalled, errors = installer.get_uninstalled(packages, implicit=options.recursive, verbose=options.verbose)
-        
+
     if options.verbose:
         print("uninstalled dependencies are: [%s]"%(', '.join([', '.join(pkg) for pkg in [v for k,v in uninstalled]])))
-        
+
     if errors:
-        _msg_no_rosdistro_envvar = None
+        err_msg = ("ERROR: the following packages/stacks could not have their "
+                   "rosdep keys resolved\nto system dependencies")
         if rospkg.distro.current_distro_codename() is None:
-            _msg_no_rosdistro_envvar = " (Could not find current distro codename. Make sure `ROS_DISTRO` environment variable is set, or use `--rosdistro` option to specify the distro (eg. `--rosdistro indigo`))"
-        print("ERROR: the following packages/stacks could not have their rosdep keys resolved\nto system dependencies" + _msg_no_rosdistro_envvar + ":", file=sys.stderr)
+            err_msg += (
+                " (ROS distro is not set. "
+                "Make sure `ROS_DISTRO` environment variable is set, or use "
+                "`--rosdistro` option to specify the distro, "
+                "e.g. `--rosdistro indigo`)"
+            )
+        print(err_msg + ":", file=sys.stderr)
         for rosdep_key, error in errors.items():
             print("%s: %s"%(rosdep_key, error_to_human_readable(error)), file=sys.stderr)
         if options.robust:
