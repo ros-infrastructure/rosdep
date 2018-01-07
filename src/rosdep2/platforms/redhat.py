a0fcbb384d5ce241bfa6f98fe3535b156fa80e7d
Caleb Jamison <cbjamo@gmail.com>
2018-01-07 07:25:52 -0500

    Fix detection of installed rpms and warn if slow method is being used
    The fallback method for detecting installed rpms (rpm_detect_cmd) was returning a list of all packages passed to it instead of only those installed.
    On line 92, the .split() was breaking up an error, "no package provides <package>", that rpm outputs.
    The list 'out_lines' then had an element for every package passed into the function, so the check on line 94 always returns true.
    
    The fix is to split on newlines so that the error is a single element in the 'out_lines' list.
    The string check on line 94 then fails on packages that aren't installed, as intended.
    
    An error message was also added to warn the user that the fallback method is being used if debugging is enabled.

--- src/rosdep2/platforms/redhat.py
+++ src/rosdep2/platforms/redhat.py
@@ -89,7 +89,7 @@ def rpm_detect_cmd(raw_packages, exec_fn=None):
     cmd.extend(packages)
 
     std_out = exec_fn(cmd)
-    out_lines = std_out.split()
+    out_lines = std_out.split('\n')
     for index, package in enumerate(packages):
         if package in out_lines:
             ret_list.append(raw_packages[index])
@@ -99,6 +99,7 @@ def rpm_detect(packages, exec_fn=None):
     try:
         return rpm_detect_py(packages)
     except ImportError:
+        rd_debug('Failed to import rpm module, falling back to slow method')
         return rpm_detect_cmd(packages, exec_fn)
 
 def rpm_expand_py(macro):
