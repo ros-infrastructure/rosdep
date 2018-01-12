695bf03a71d0149defca74588b08cf92f929bd1f
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-01-12 12:00:43 -0800

    skip flake8 in Python 2.6

--- test/test_flake8.py
+++ test/test_flake8.py
@@ -17,10 +17,18 @@ from __future__ import print_function
 import os
 import sys
 
-from flake8.api.legacy import get_style_guide
+# flake8 doesn't support Python < 2.7 anymore
+if sys.version_info[0] > 2 or sys.version_info[1] >= 7:
+    from flake8.api.legacy import get_style_guide
+else:
+    get_style_guide = None
 
 
 def test_flake8():
+    if get_style_guide is None:
+        # skip test on Python 2.6 and older
+        return
+
     style_guide = get_style_guide(
         exclude=['conf.py'],
         ignore=[
