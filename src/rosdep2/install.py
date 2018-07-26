579cd91a990b881992ee1111002abd6e6c6afc3f
Lou Amadio <ooeygui@users.noreply.github.com>
2018-07-26 14:36:43 -0700

    Add console script entry point
    Add console script entry point for platforms (e.g. Windows) not supporting shebang.

--- src/rosdep2/install.py
+++ src/rosdep2/install.py
@@ -26,5 +26,35 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from rosdep2.install import install_main
-install_main()
+"""
+Script for installing rdmanifest-described resources
+"""
+
+from __future__ import print_function
+
+import os
+import sys
+from optparse import OptionParser
+
+from rosdep2 import InstallFailed
+from rosdep2.platforms import source
+
+NAME = 'rosdep-source'
+
+
+def install_main():
+    parser = OptionParser(usage="usage: %prog install <rdmanifest-url>", prog=NAME)
+    options, args = parser.parse_args()
+    if len(args) != 2:
+        parser.error("please specify one and only one rdmanifest url")
+    if args[0] != 'install':
+        parser.error("currently only support the 'install' command")
+    rdmanifest_url = args[1]
+    try:
+        if os.path.isfile(rdmanifest_url):
+            source.install_from_file(rdmanifest_url)
+        else:
+            source.install_from_url(rdmanifest_url)
+    except InstallFailed as e:
+        print("ERROR: installation failed:\n%s" % e, file=sys.stderr)
+        sys.exit(1)
