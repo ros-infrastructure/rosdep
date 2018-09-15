265e5c30a8007e6caba7e5eb82839e66a50532e9
William Woodall <william@osrfoundation.org>
2018-09-14 17:36:06 -0700

    remove special cases for homebrew
    Signed-off-by: William Woodall <william@osrfoundation.org>

--- .travis/install.sh
+++ .travis/install.sh
@@ -12,17 +12,6 @@ do_install()
         PYTHON_ENV_NAME=virtual-env-$TRAVIS_PYTHON_VERSION
         pyenv virtualenv $TRAVIS_PYTHON_VERSION $PYTHON_ENV_NAME
         pyenv activate $PYTHON_ENV_NAME
-
-    elif [[ $TRAVIS_OS_NAME == 'osx' && $PYTHON_INSTALLER == 'brew' ]]; then
-        # nose 1.3.7 creates /usr/local/man dir if it does not exist.
-        # The operation fails because current user does not own /usr/local.  Create the dir manually instead.
-        sudo mkdir /usr/local/man
-        sudo chown -R $(whoami) $(brew --prefix)/*
-
-        export PATH=$(pwd)/.travis/shim:$PATH
-
-        mkdir -p ~/Library/Python/2.7/lib/python/site-packages
-        echo "$(brew --prefix)/lib/python2.7/site-packages" >> ~/Library/Python/2.7/lib/python/site-packages/homebrew.pth
     fi
 }
 
