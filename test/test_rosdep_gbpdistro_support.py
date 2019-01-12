bdbbdb2b38cec039b6e3f1827e6c4af026a2c7a3
Jorge Rodriguez <veimox@gmail.com>
2019-01-12 18:36:44 +0100

    Fix the unit test test_get_gbprepo_as_rosdep_data
    The unit test checks for the existance of thre keys ['ros', 'catkin', 'genmsg'] in the rosdep data gather from a distro. As coded, it gets the first distribution found in alphabetical order. As of today "ardent". However this is a ros2 distribution and, for example, catkin is not released for it  (https://github.com/ros/rosdistro/blob/master/ardent/distribution.yaml)

--- test/test_rosdep_gbpdistro_support.py
+++ test/test_rosdep_gbpdistro_support.py
@@ -52,7 +52,7 @@ def test_url_constants():
 def test_get_gbprepo_as_rosdep_data():
     from rosdep2.rosdistrohelper import get_index
     from rosdep2.gbpdistro_support import get_gbprepo_as_rosdep_data
-    distro = sorted(get_index().distributions.keys())[0]
+    distro = "melodic"
     data = get_gbprepo_as_rosdep_data(distro)
     for k in ['ros', 'catkin', 'genmsg']:
         assert k in data, data
