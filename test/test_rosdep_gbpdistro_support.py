1d3a35f9272f8ae56bdefe58e6b2f19c0d1685e0
Steven! Ragnarök <steven@nuclearsandwich.com>
2019-01-23 12:36:08 -0800

    Use a more descriptive internal variable name.

--- test/test_rosdep_gbpdistro_support.py
+++ test/test_rosdep_gbpdistro_support.py
@@ -53,7 +53,7 @@ def test_get_gbprepo_as_rosdep_data():
     from rosdep2.rosdistrohelper import get_index
     from rosdep2.gbpdistro_support import get_gbprepo_as_rosdep_data
     distro = sorted(
-        d for d, info in get_index().distributions.items()
+        name for name, info in get_index().distributions.items()
         if info['distribution_type'] == 'ros1')[0]
     data = get_gbprepo_as_rosdep_data(distro)
     for k in ['ros', 'catkin', 'genmsg']:
