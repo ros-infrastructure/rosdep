527de9fc615d4f2d6ddc56d12fbd89e4d22d3045
Mikael Arguedas <mikael.arguedas@gmail.com>
2019-10-24 18:50:24 +0200

    Indent printed rosdep db extract for null keys
    Co-Authored-By: Scott K Logan <logans@cottsay.net>

--- src/rosdep2/lookup.py
+++ src/rosdep2/lookup.py
@@ -174,7 +174,7 @@ class ResolutionError(Exception):
 \trosdep key : %s
 \tOS name    : %s
 \tOS version : %s
-\tData:\n%s""" % (self.args[0], self.rosdep_key, self.os_name, self.os_version, pretty_data)
+\tData:\n%s""" % (self.args[0], self.rosdep_key, self.os_name, self.os_version, pretty_data.replace('\n', '\n\t\t'))
 
 
 class RosdepView(object):
