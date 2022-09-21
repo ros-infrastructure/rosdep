b1192784b462657f8c30341ab536940ed65b9e3a
Scott K Logan <logans@cottsay.net>
2022-09-21 14:09:06 -0700

    Add missing import for Python 2

--- src/rosdep2/url_utils.py
+++ src/rosdep2/url_utils.py
@@ -25,6 +25,8 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+from __future__ import print_function
+
 import base64
 import sys
 from gzip import GzipFile
