b68c72c614effd216eaac1753dc006675e37973d
SubaruArai <slarai@minebeamitsumi.com>
2023-08-17 13:37:11 +0900

    Use setup.cfg to configure flake8 !has regression!
    This makes flake8's configuration more discoverable and easier to
    use with other tools (IDE, etc.).
    
    Regression: it seems that "max-complexity" wasn't properly enforced,
    now a lot of code is detected as too complex. (warn if > 10, max is 25)
    We may need to adjust the value.

--- test/test_flake8.py
+++ test/test_flake8.py
@@ -14,50 +14,23 @@
 
 from __future__ import print_function
 
-import os
+import pathlib
+import subprocess
 import sys
 
-from flake8.api.legacy import get_style_guide
 import pytest
 
 
 @pytest.mark.flake8
 @pytest.mark.linter
 def test_flake8():
-    style_guide = get_style_guide(
-        exclude=['conf.py'],
-        ignore=[
-            'C402',  # ignore presence of unnecessary generators
-            'C405',  # ignore presence of unnecessary literals
-            'C407',  # ignore presence of unnecessary comprehensions
-            'C408',  # ignore presence of unnecessary tuple/list/dict
-            'D',  # ignore documentation related warnings
-            'F401',  # ignore presence of unused imports
-            'F841',  # ignore presence of unused variables
-            'I',  # ignore import order related warnings
-            'N802',  # ignore presence of upper case in function names
-            'W504',  # ignore line breaks after binary operator (new rule added in 2018)
-        ],
-        max_line_length=200,
-        max_complexity=10,
-        show_source=True,
+    result = subprocess.run(
+        ['flake8'],
+        cwd=pathlib.Path(__file__).parent.parent,
+        capture_output=True,
+        check=False,
     )
-
-    stdout = sys.stdout
-    sys.stdout = sys.stderr
-    # implicitly calls report_errors()
-    report = style_guide.check_files([
-        os.path.dirname(os.path.dirname(__file__)),
-    ])
-    sys.stdout = stdout
-
-    if report.total_errors:
-        # output summary with per-category counts
-        print()
-        report._application.formatter.show_statistics(report._stats)
-        print(
-            'flake8 reported {report.total_errors} errors'
-            .format(**locals()), file=sys.stderr)
-
-    assert not report.total_errors, \
-        'flake8 reported {report.total_errors} errors'.format(**locals())
+    # pipe stdout to stdout, stderr to stderr
+    print(result.stdout.decode('utf-8'), end='')
+    print(result.stderr.decode('utf-8'), end='', file=sys.stderr)
+    assert 0 == result.returncode, "flake8 found violations"
